016f918e6f0cb5ad74f2ba895b427369131b4a91
ctang415 <carrietang117@gmail.com>
2022-11-29 10:09:04 -0500

    Added babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,30 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.m?js$/,
+        exclude: {
+          and: [/node_modules/],
+          not: [
+            /unfetch/,
+            /d3-array|d3-scale/,
+            /@hapi[\\/]joi-date/,
+          ]
+        },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            "exclude": [
+              /node_modules[\\/]core-js/,
+              /node_modules[\\/]webpack[\\/]buildin/,
+            ],
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      },
     ],
   },
 };
